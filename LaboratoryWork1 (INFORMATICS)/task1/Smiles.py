bbde76f1a25d5d32d6053cbf2970021b8d346196
Dmitriy Busygin <Busygin.voll@mail.ru>
2021-10-19 00:52:05 +0300

    Update Smiles.py

--- LaboratoryWork1 (INFORMATICS)/task1/Smiles.py
+++ LaboratoryWork1 (INFORMATICS)/task1/Smiles.py
@@ -1,13 +1,13 @@
 # ISU = 335103
 # eyes = 3   nose = 3   mouth = 6
 import re
-def CountOfSmiles(filename):
-    mySmile = re.compile(r'8<{P')
-    return len(re.findall(mySmile, filename))
+def count_of_smiles(filename):
+    my_smile = re.compile(r'8<{P')
+    return len(re.findall(my_smile, filename))
 
 for i in range(1,6):
     fin = open('test' + str(i) + '.txt').read()
-    print(CountOfSmiles(fin))
+    print(count_of_smiles(fin))
 
 # Ответы, полученные без использования программы:
 # 1: 49
