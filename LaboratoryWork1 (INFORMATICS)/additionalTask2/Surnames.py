6218fca679913a97325b9fcf25ac0d96583e5d26
Dmitriy Busygin <Busygin.voll@mail.ru>
2021-10-20 15:01:07 +0300

    Update Surnames.py

--- LaboratoryWork1 (INFORMATICS)/additionalTask2/Surnames.py
+++ LaboratoryWork1 (INFORMATICS)/additionalTask2/Surnames.py
@@ -6,7 +6,7 @@ def surnames_list_from_file(filename):
     surnames = []
     rus_alphabet = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
     for string in filename:
-        fios = re.findall(r'[А-ЯЁ][а-яё]* [А-ЯЁ]\.[А-ЯЁ]\.', string)
+        fios = re.findall(r'[А-ЯЁ](?:[а-яё]|-)* [А-ЯЁ]\.[А-ЯЁ]\.', string)
         for elem in fios:
             surnames.extend(elem.split()[::2])
     surnames.sort(key=lambda x: rus_alphabet.index(x[0]))
@@ -30,6 +30,7 @@ for i in range(1,6):
 # 2: Баженов
 #    Бецкой
 #    Деламот
+#    Кваренги
 #    Растрелли
 #    Старов
 #    Фельтен
